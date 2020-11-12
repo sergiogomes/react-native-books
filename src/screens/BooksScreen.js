2d7001b53e860d845f3b610cf6a78a54b0deba7e
sergiogomes <sergiopgomes92@gmail.com>
2020-11-11 21:24:06 -0300

    Creating navigator stack and a Book Screen page.

---
+++ src/screens/BooksScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const BooksScreen = () => {
+  return (
+    <View>
+      <Text>Books Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default BooksScreen;
